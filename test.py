e865f23a2ea52f6388c5f9e715c831b41ac14fc8
chehualiu <chehua@gmail.com>
2021-06-02 15:49:15 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,25 @@
+print('test')
+
+# git config --global user.email "chehua@gmail.com"
+
+# git init
+# git add .  # add everything.
+# git status  show tracked and untracked files.
+# git rm rm --cached <file>
+# git commit -m "my first commit"
+# git branch -M main  # rename master branch to main. recent change in github.
+# git checkout -b new-feature
+# git checkout master
+# git remote add origin https://github.com/chehualiu/helloworld.git
+# git push -u origin main
+
+# git remote
+# git push -u origin main
+
+# git checkout extended
+# git push orign extended
+
+# git pull   # update local from remote
+
+# git clone https://gitub.....git       git origin set to this new URL.
+# 
