3ee974ade519ee7e645d580625b5a26c3311a63c
chehualiu <chehua@gmail.com>
2024-09-19 14:15:24 +0800

    v.2.5B

--- monitor_v2.5.py
+++ monitor_v2.5.py
@@ -1710,9 +1710,9 @@ if __name__ == '__main__':
     factor = factor+[1.00]
 
     drawAllCCBmin1A5B()
-    # plotAllzjlx()
+    plotAllzjlx()
 
-    # main()
+    main()
 
     tdxdata.api.close()
     tdxdata.Exapi.close()
